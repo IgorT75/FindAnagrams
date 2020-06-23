26face0b3c4d6fbf1b2e971739a88fbcc494aa85
Igor <i.trubnikov75@gmail.com>
2020-06-22 22:35:58 -0700

    Working version

--- findAnagrams.cpp
+++ findAnagrams.cpp
@@ -3,19 +3,49 @@
 #include <algorithm>
 #include <vector>
 #include <unordered_map>
-#include <map>
 
 using namespace std;
 
 // https://leetcode.com/problems/find-all-anagrams-in-a-string/
 vector<int> findAnagrams(string s, string p) {
-	vector<int> ret;
-	if (p.size() == 1) {
-		for (size_t i = 0, n = s.size(); i < n; ++i)
-			if (s[i] == p.front())
-				ret.push_back(i);
+	unordered_map<char, size_t> charCount;
+	for (char c : p)
+		charCount[c]++;
 
-		return ret;
+	vector<int> ret;
+	unordered_map<char, size_t> charOccur;
+	size_t l = 0, r = 0, total = 0;
+	while (l <= r && r < s.size()) {
+		char c = s[r];
+
+		// is in pattern
+		if (p.find(c) != string::npos) {
+			charOccur[c]++;
+			++total;
+
+			if (charOccur[c] == charCount[c]) {
+				if (total == p.size()) {
+					ret.push_back(l);
+
+					// push l one symbol
+					charOccur[s[l]]--;
+					++l; --total;
+				}
+			}
+			else if (charOccur[c] > charCount[c]) {
+				// push l until we encounter c
+				do {
+					charOccur[s[l]]--;
+					--total;
+				} while (s[l++] != c && l < r);
+			}
+			++r;
+		}
+		else { // not in pattern
+			l = r = r + 1;
+			total = 0;
+			charOccur.clear();
+		}
 	}
 
 	return ret;
